a37ca47a443cba4a1c7c3da90d86d788edc057ad
Kumar Kothapalli <P@ssword2012>
2013-12-16 14:32:12 -0700

    Completed User edit, update, index and destroy actions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -11,7 +11,7 @@ class SessionsController < ApplicationController
 		  if user && user.authenticate(params[:session][:password]) #3 possible results with this logic
 		    # Sign the user in and redirect to the user's show page.
 		    sign_in user #sign_in function
-		    redirect_to user
+		    redirect_back_or user # redict to requested URL if it exists or to some default URL
 		  else
 		    # Create an error message and re-render the signin form.
 		    # flash.now is used to display flash messages on rendered pages
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,15 @@
 class UsersController < ApplicationController
+  # invoking signed_in_user method.
+  # Filter acts only on edit and update.
+  before_action :signed_in_user, only: [:index, :edit, :update]
+  before_action :correct_user,   only: [:edit, :update]
+  before_action :admin_user,     only: :destroy
   
+  def index
+    @users = User.paginate(page: params[:page]) # Key: page, value is page
+    # 30 is the default number of users pulled out.
+  end
+
   def show
   	@user = User.find(params[:id]) # params used to retrieve user id.
   end
@@ -22,8 +32,46 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    # @user = User.find(params[:id])
+  end
+
+  def update
+    # @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+  # Only admins can destroy as written in _user.html.erb
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted."
+    redirect_to users_url
+  end
+
   private
+
     def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end
+
+    # Before filters 
+    def signed_in_user
+      unless signed_in?
+        store_location
+      redirect_to signin_url, notice: "Please sign in."
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
+    end
+    
+    def admin_user
+      redirect_to(root_url) unless current_user.admin?
+    end
+end
 end
